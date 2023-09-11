5ead58fc7a3fa75784bf0e61c99804e3d6f91312
NotBii <costic1375@gmail.com>
2023-09-11 13:38:01 +0900

    controller 설계

--- src/main/java/com/zerobase/stock/StockApplication.java
+++ src/main/java/com/zerobase/stock/StockApplication.java
@@ -3,6 +3,8 @@ package com.zerobase.stock;
 import org.jsoup.Connection;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,12 +14,30 @@ import java.io.IOException;
 public class StockApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(StockApplication.class, args);
+//        SpringApplication.run(StockApplication.class, args);
 
         try {
-            Connection connection = Jsoup.connect("https://finance.yahoo.com/quote/COKE/history?period1=99100800&period2=1694304000&interval=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
+            Connection connection = Jsoup.connect("https://finance.yahoo.com/" +
+                    "quote/COKE/history?period1=99100800&period2=1694304000&interval" +
+                    "=1mo&filter=history&frequency=1mo&includeAdjustedClose=true");
             Document document = connection.get();
 
+            Elements eles = document.getElementsByAttributeValue("data-test", "historical-prices");
+            Element ele = eles.get(0);
+
+            Element tbody = ele.children().get(1);
+            for (Element e : tbody.children()) {
+                String txt = e.text();
+                if (!txt.endsWith("Dividend")) {
+                    continue;
+                }
+                String[] splits = txt.split(" ");
+                String month = splits[0];
+                int day = Integer.valueOf(splits[1].replace(",", ""));
+                int year = Integer.valueOf(splits[2]);
+                String dividend = splits[3];
+                System.out.println(year + "/" + month + "/" + day + " -> " + dividend);
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
